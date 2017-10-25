d788c5b7af2794dd09a23b8cb62b85ea8c80c815
iceboy95 <hqkhor95@EMAIL.com>
2017-10-25 19:09:36 +0800

    added predicted labels

--- External-Tools/keras-cam/model.py
+++ External-Tools/keras-cam/model.py
@@ -17,50 +17,6 @@ from keras import metrics
 from keras import backend as K
 from keras.layers import GlobalAveragePooling2D
 
-def global_average_pooling(x):
-	return K.mean(x, axis = (2, 3))
-
-def global_average_pooling_shape(input_shape):
-	return input_shape[0:2]
-
-def VGG16_convolutions():
-	model = Sequential()
-	model.add(ZeroPadding2D((1,1),input_shape=(3,None,None)))
-	model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-	model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-	model.add(ZeroPadding2D((1, 1)))
-	model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-	return model
-
 def VGG_16(weights_path=None):
 	model = Sequential()
 	model.add(ZeroPadding2D((1,1),input_shape=(3, 224, 224)))
@@ -114,18 +70,7 @@ def VGG_16(weights_path=None):
 	
 	return model
 
-def get_model():
-	model = VGG16_convolutions()
 
-	model = load_model_weights(model, "vgg16_weights.h5")
-	
-	model.add(Lambda(global_average_pooling, 
-			  output_shape=global_average_pooling_shape))
-
-	model.add(Dense(2, activation = 'softmax', init = 'uniform'))
-	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.5, nesterov=True)
-	model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics=['accuracy'])
-	return model
 
 def modify_model(weights_path):
 	model = VGG_16(weights_path)
@@ -135,13 +80,11 @@ def modify_model(weights_path):
 	model.pop()
 	model.pop()
 	model.pop()	
-	# model.add(Lambda(global_average_pooling,
-	# 		  output_shape = global_average_pooling_shape))	
+
 	model.add(GlobalAveragePooling2D(data_format='channels_first'))
 	model.add(Dense(5, activation = 'softmax'))
 	adam = optimizers.Adam(lr=0.00001)
 
-	# sgd = SGD(lr = 0.0001, decay = 1e-6, momentum = 0.5, nesterov = True)
 	model.compile(loss = 'categorical_crossentropy', optimizer = adam, metrics = [metrics.categorical_accuracy])
 	return model
 
